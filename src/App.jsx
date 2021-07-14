9ef9513955a47c2445de6ec32762f76d6e1eebfc
Howard Jiang <howardrrjgo@gmail.com>
2021-07-14 16:00:08 +1200

    set and clear message

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { Navbar, Container, Row, Col, Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import InputMessage from './InputMessage';
+import { useEffect, useState } from 'react';
+
+const message = {
+  content: "Hello"
+}
+
+const App = () => {
+
+  const [messages, setMessages] = useState([message, message])
+
+  const addMessage = (msg) => {
+    setMessages([...messages, msg])
+  }
+
+  const clearMessage = () => {
+    setMessages([""])
+  }
+
+  useEffect(() => {
+    console.log("called on start")
+    //to call backend 
+    // on component mount
+  }, [])
+
+  useEffect(() => {
+    console.log("message changed")
+  }, [messages])
+
+  return (
+    <Container fluid>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Billboard</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+
+
+      <Container style={{ maxWidth: '1000px', margin: 'auto' }}>
+        {/* <Button onClick={() => setValue(value + 1)}>Increment</Button>
+        <Button onClick={() => setValue(value - 1)}>Decrement</Button> */}
+        <Row>
+          <Col>
+            <InputMessage label="Message" placeholder="Enter Message" onSubmit={addMessage} />
+          </Col>
+          <Col>
+            <InputMessage label="Search" placeholder="Enter Search" onSubmit={clearMessage} />
+          </Col>
+        </Row>
+
+
+        {messages?.map(message =>
+          <Container>{message.content}</Container>
+        )}
+      </Container>
+
+
+
+    </Container>
+  );
+}
+
+
+
+export default App;
---
+++ src/InputMessage.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+const InputMessage = ({ label, placeholder, onSubmit }) => {
+
+    const [input, setInput] = useState("")
+
+    const handleSubmit = () => {
+
+    }
+
+    return (
+        <Form>
+            <Form.Group>
+                <Form.Label>{label}</Form.Label>
+                <Form.Control value={input} onChange={e => setInput(e.target.value)} placeholder={placeholder} />
+            </Form.Group>
+
+            <Button variant="primary" onClick={() => onSubmit({ content: input })}>
+                Submit
+            </Button>
+        </Form>
+    )
+}
+
+export default InputMessage
