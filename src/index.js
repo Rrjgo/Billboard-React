86fe579799497034a1ec7c2f6b5ad68b197429a7
Rrjgo <howardrrjgo@gmail.com>
2021-07-20 00:22:05 +1200

    message get from backend

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const axios = require('axios')
+
+const endpoint = 'https://billboard-backend-hj.herokuapp.com'
+
+axios.get(`${endpoint}/msg`)
+  .then(data => {
+    // handle success
+    
+    console.log(data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
